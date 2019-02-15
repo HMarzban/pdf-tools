7b9b5fe5de31613720122cee95265d17c6924069
Hossein Marzban <marzban98@gmail.com>
2019-02-15 22:44:34 +0330

    Revert "as"
    This reverts commit ff0bb2e76fb64fde2da0b322833f42331d7156ca.

--- server/api/pdf/compress/compress-route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { upload } = require('../_middleware');
-const controller = require('./compress-controller');
-
-// api/pdf/compress
-router.post('/compress', upload.single('pdf'), controller);
-
-module.exports = router;
--- server/api/pdf/merge/merge-route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { upload } = require('../_middleware');
-const controller = require('./merge-controller');
-
-// api/pdf/merge
-router.post('/merge', upload.array('pdf', 10), controller);
-
-module.exports = router;
